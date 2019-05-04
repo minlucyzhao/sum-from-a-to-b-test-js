b3d9de15d51177d77e20295b261c5383614d8d1d
minlucyzhao <minlucyzhao@gmail.com>
2019-05-04 14:48:32 +0000

    Add sum function recrusively

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if (fromN <= 0 && toN <= 0 || toN < 0 || fromN > toN) {
+    return 0;
+  }
+  return toN + sum(fromN, toN-1);
 }
 
 module.exports = sum;
